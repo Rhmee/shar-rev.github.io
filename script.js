6b76d1a24b1612812a3708252a1bd6b9064add85
Rhmee <erhembata@gmail.com>
2023-11-17 00:02:43 +0800

    slides

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-// Get the movie ID from the URL
-const urlParams = new URLSearchParams(window.location.search);
-const movieId = urlParams.get('movieId');
+let slideIndex = 0;
+showSlides();
 
-// Fetch the specific movie details from JSON
-fetch('movies.json')  // Adjust the path based on your file structure
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        const movie = data.movies[movieId]; // Adjusting for array index
-        const movieDetailsElement = document.getElementById('movie-details');
-        movieDetailsElement.innerHTML = `
-            <li>${movie.ner}</li>
-            <li>Төрөл: ${movie.genre}</li>
-            <li>Гарсан огноо: ${movie.release_date}</li>
-            <li>Үргэлжлэх хугацаа: ${movie.urgeljleh_hugatsaa}</li>
-            <li>Насны ангилал: ${movie.nasnii_angilal}</li>
-            <li>Товч агуулга: ${movie.tovch_aguulga}</li>
-            <li>Найруулагч: ${movie.nairuulagch}</li>
-            <li>Продюсер: ${movie.producer}</li>
-            <li>Жүжигчид: ${movie.jujigchid.join(', ')}</li>
-        `;
-    })
-    .catch(error => console.error('Error fetching data:', error));
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+document.querySelector('.prev').addEventListener('click', function () {
+    plusSlides(-1);
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    plusSlides(1);
+});
+
+
+function showSlides() {
+    const slides = document.getElementsByClassName("slidepic");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 5000);
+}
+
