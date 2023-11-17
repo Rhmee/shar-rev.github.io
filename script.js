fc8077bd398556024a422d8ce4853cba3230457f
Anuyb <132063608+Anuyb@users.noreply.github.com>
2023-11-17 12:04:50 +0800

    js

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
 // Get the movie ID from the URL
 const urlParams = new URLSearchParams(window.location.search);
-const movieId = urlParams.get('movieId');
+const movieId = urlParams.get('id');
 
 // Fetch the specific movie details from JSON
 fetch('movies.json')  // Adjust the path based on your file structure
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        const movie = data.movies[movieId]; // Adjusting for array index
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+})    
+.then(data => {
+        const movie = data.movies.find((i) => i.id == movieId); // Adjusting for array index
         const movieDetailsElement = document.getElementById('movie-details');
         movieDetailsElement.innerHTML = `
-            <li>${movie.ner}</li>
-            <li>Төрөл: ${movie.genre}</li>
-            <li>Гарсан огноо: ${movie.release_date}</li>
-            <li>Үргэлжлэх хугацаа: ${movie.urgeljleh_hugatsaa}</li>
-            <li>Насны ангилал: ${movie.nasnii_angilal}</li>
-            <li>Товч агуулга: ${movie.tovch_aguulga}</li>
-            <li>Найруулагч: ${movie.nairuulagch}</li>
-            <li>Продюсер: ${movie.producer}</li>
-            <li>Жүжигчид: ${movie.jujigchid.join(', ')}</li>
+        <article class="heseg">
+            <img id="Poster_zurag" src="${movie.poster}" alt="${movie.ner} Poster">
+        </article>
+        <article class="heseg">
+            <h2>${movie.ner}</h2>
+            <p class="unelgee">
+                <span>127K <i class="fa fa-eye" aria-hidden="true"></i></span>
+                <span style="color: #ffd747;">&#124;</span>
+                <span>8 <i class="fa fa-star" style="color: #ffd747;"></i></span>
+                <span style="color: #ffd747;">&#124;</span>
+                <span>10 <i class="fa fa-heart" style="color:red"></i></span>
+            </p>
+            <p>Tөрөл: <span>${movie.genre}</span></p>
+            <p>Үргэжлэх хугацаа: <span>${movie.urgeljleh_hugatsaa}</span></p>
+            <p>Насны ангилал: <span>${movie.nasnii_angilal}</span></p>
+            <p>${movie.tovch_aguulga}</p>
+            <p>Найруулагч: <span class="medeelel">${movie.nairuulagch}</span></p>
+            <p>Продюсер <span>${movie.producer}</span></p>
+            <p>Гол дүр: <span>${movie.jujigchid.join(', ')}</span></p>
+        </article>
+        <aside style="background-color: rgb(236, 214, 71)" class="heseg">
+            <input type="text" class="search" placeholder="Хайх">
+            <article>
+                <img src="./photos/posters/tushig.jpg" alt="">
+                <div>
+                    <h2>Гарчиг 1</h2>
+                    <p>Товч тайлбар</p>
+                    <p>2020/10/20</p>
+                </div>
+            </article>
+            <article>
+                <img src="./photos/posters/narrrrr.png" alt="">
+                <h2>Гарчиг 1</h2>
+                <p>Товч тайлбар</p>
+                <p>2020/10/20</p>
+            </article>
+            <article>
+                <h4>Трейлер</h4>
+                <video src="youtube.com"></video>
+            </article>
+        </aside>    
         `;
     })
     .catch(error => console.error('Error fetching data:', error));
