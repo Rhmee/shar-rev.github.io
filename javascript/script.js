d0f2f1db13df6635674241266e285c22ce0b4ae5
Rhmee <erhembata@gmail.com>
2023-12-21 03:21:07 +0800

    removed empty files, fixed position of review field

--- javascript/script.js
+++ javascript/script.js
@@ -19,52 +19,34 @@ const API_KEY = "$2a$10$xgjEuMB695YYdVlN5PlH3O71Mntu9XSGLsO3KwS0wv6NxzenB.fCW";
         const movie = data.record.movies.find((i) => i.id == movieId);
         const movieDetailsElement = document.getElementById('movie-details');
         movieDetailsElement.innerHTML = `
-        <article class="heseg">
-            <img id="Poster_zurag" src="${movie.poster}" alt="${movie.ner} Poster">
-        </article>
-        <article class="heseg">
-            <h2>${movie.ner}</h2>
-            <p class="unelgee">
-                <span>127K <i class="fa fa-eye" aria-hidden="true"></i></span>
-                <span style="color: #ffd747;">&#124;</span>
-                <span>8 <i class="fa fa-star" style="color: #ffd747;"></i></span>
-                <span style="color: #ffd747;">&#124;</span>
-                <span>10 <i class="fa fa-heart" style="color:red"></i></span>
-            </p>
-            <p><i id="icon" onclick="likebtn()" class="far fa-heart"></i></p>
-            <p>Tөрөл: <span>${movie.genre}</span></p>
-            <p>Үргэжлэх хугацаа: <span>${movie.urgeljleh_hugatsaa}</span></p>
-            <p>Гарсан огноо: <span>${movie.release_date}</span></p>
-            <p>Насны ангилал: <span>${movie.nasnii_angilal}</span></p>
-            <p>${movie.tovch_aguulga}</p>
-            <p>Найруулагч: <span class="medeelel">${movie.nairuulagch}</span></p>
-            <p>Продюсер <span>${movie.producer}</span></p>
-            <p>Гол дүр: <span>${movie.jujigchid.join(', ')}</span></p>
-        </article>
-        <script type="text/javascript">
-            let btn = document.getElementById("icon");
-            function likebtn(){
-                if(btn.classList.contains("far")){
-                    btn.classList.remove("far");
-                    btn.classList.add("fas");
-                    btn.style.color = "red";
-                }
-                else{
-                    btn.classList.remove("fas");
-                    btn.classList.add("far");
-                    btn.style.color = ""; 
-                }
-            }        
-        </script>
-        <aside class="hesegbish">
-        <input type="text" class="search" placeholder="Хайх">
-        <section>
-            <article class="jijigReview">
-                <img class="proff" src="/photos/posters/tushig.jpg" alt="">
-                <div>
-                    <h4 class="rewInfo">Гарчиг 1</h2>
-                    <p class="rewInfo">Товч тайлбар</p>
-                    <p class="yDay">2020/10/20</p>
+            <article class="heseg">
+                <img id="Poster_zurag" src="${movie.poster}" alt="${movie.ner} Poster">
+            </article>
+            <article class="heseg">
+                <h2>${movie.ner}</h2>
+                <p class="unelgee">
+                    <span>127K <i class="fa fa-eye" aria-hidden="true"></i></span>
+                    <span style="color: #ffd747;">&#124;</span>
+                    <span>8 <i class="fa fa-star" style="color: #ffd747;"></i></span>
+                    <span style="color: #ffd747;">&#124;</span>
+                    <span>10 <i class="fa fa-heart" style="color:red"></i></span>
+                </p>
+                <p><i id="icon" onclick="likebtn()" class="far fa-heart"></i></p>
+                <p>Tөрөл: <span>${movie.genre}</span></p>
+                <p>Үргэжлэх хугацаа: <span>${movie.urgeljleh_hugatsaa}</span></p>
+                <p>Гарсан огноо: <span>${movie.release_date}</span></p>
+                <p>Насны ангилал: <span>${movie.nasnii_angilal}</span></p>
+                <p>${movie.tovch_aguulga}</p>
+                <p>Найруулагч: <span class="medeelel">${movie.nairuulagch}</span></p>
+                <p>Продюсер <span>${movie.producer}</span></p>
+                <p>Гол дүр: <span>${movie.jujigchid.join(', ')}</span></p>
+                <h3>Шүүмж бичих</h3>
+                <div class="reviewing" id="review">
+                    <fieldset class="rating-container">
+                        ${generateStarRatingHTML()}
+                        <div class="rating-value"></div>
+                    </fieldset>
+                    <input type="text" id="inputRev">
                 </div>
             </article>`;
     })
