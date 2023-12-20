cdf77021b1b0558f0f0ff432bf0aa1e11dc7f046
Rhmee <erhembata@gmail.com>
2023-12-21 05:37:01 +0800

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -25,11 +25,11 @@ const API_KEY = "$2a$10$xgjEuMB695YYdVlN5PlH3O71Mntu9XSGLsO3KwS0wv6NxzenB.fCW";
             <article class="heseg">
                 <h2>${movie.ner}</h2>
                 <p class="unelgee">
-                    <span>127K <i class="fa fa-eye" aria-hidden="true"></i></span>
+                    <span>${movie.views}<i class="fa fa-eye" aria-hidden="true"></i></span>
                     <span style="color: #ffd747;">&#124;</span>
-                    <span>8 <i class="fa fa-star" style="color: #ffd747;"></i></span>
+                    <span>${movie.rate}<i class="fa fa-star" style="color: #ffd747;"></i></span>
                     <span style="color: #ffd747;">&#124;</span>
-                    <span>10 <i class="fa fa-heart" style="color:red"></i></span>
+                    <span>${movie.rate}<i class="fa fa-heart" style="color:red"></i></span>
                 </p>
                 <p><i id="icon" onclick="likebtn()" class="far fa-heart"></i></p>
                 <p>Tөрөл: <span>${movie.genre}</span></p>
@@ -38,7 +38,7 @@ const API_KEY = "$2a$10$xgjEuMB695YYdVlN5PlH3O71Mntu9XSGLsO3KwS0wv6NxzenB.fCW";
                 <p>Насны ангилал: <span>${movie.nasnii_angilal}</span></p>
                 <p>${movie.tovch_aguulga}</p>
                 <p>Найруулагч: <span class="medeelel">${movie.nairuulagch}</span></p>
-                <p>Продюсер <span>${movie.producer}</span></p>
+                <p>Продюсер: <span>${movie.producer}</span></p>
                 <p>Гол дүр: <span>${movie.jujigchid.join(', ')}</span></p>
                 <h3>Шүүмж бичих</h3>
                 <div class="reviewing" id="review">
