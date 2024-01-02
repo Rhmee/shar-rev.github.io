58cb5d26aaf584288f3721c7cd6a33aad30ae73c
Anuyb <132063608+Anuyb@users.noreply.github.com>
2024-01-02 23:47:11 +0800

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -14,9 +14,10 @@ class App {
     }
 
     async init() {
+        this.showSkeletonLoading();
         const newMovies = await this.network.fetchMovies();
         const topMovies = await this.network.fetchMovies();
-
+        this.hideSkeletonLoading();
         if (newMovies) {
             const sortedNewMovies = this.sortMoviesByReleaseDate(newMovies);
             this.displayMoviesNew(this.newMoviesSection, sortedNewMovies);
@@ -32,8 +33,193 @@ class App {
         this.startAutoSlide(); 
     }
 
+
+    showSkeletonLoading() {
+        const skeletonLoadingHTML = `
+        <style>
+        .slide {
+            display: none;
+        }
+
+        .section-new .next {
+            display: none;
+        }
+
+        .section-review,
+        .news,
+        h2 {
+            display: none;
+        }
+
+
+        .movies-content {
+            // margin-left: 10%;
+            // margin-right: 10%;
+            background-color: #333333;
+            color: rgb(255, 255, 255);
+            position: relative;
+            z-index: -1;
+            min-width: 1400px;
+            margin: auto;
+        }
+
+        .slidee {
+            position: relative;
+            top: -30px;
+            width: 100%;
+            height: 500px;
+            background-color: #555;
+        }
+
+        .section-neww .ug {
+            display: inline-block;
+        }
+
+        .section-neww,
+        .section-revieww {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+        }
+
+        .neg {
+            width: 175px;
+            height: 275px;
+            background-color: #555;
+            border-radius: 15px;
+            margin-bottom: 10px;
+        }
+
+        .img {
+            width: 95px;
+            height: 140px;
+            background-color: #555;
+            border-radius: 15px;
+        }
+
+        .new-movies {
+            display: flex;
+            gap: 20px;
+            justify-content: center;
+        }
+
+        .ug {
+            width: 175px;
+            height: 20px;
+            background-color: #555;
+            border-radius: 5px;
+            /* margin-right: 200px; */
+        }
+    
+        .review .article {
+            margin-top: 10px;
+            display: grid;
+            grid-template-columns: auto auto;
+        }
+
+        .h3 {
+            width: 180px;
+            height: 20px;
+            background-color: #555;
+            border-radius: 5px;
+        }
+
+        .jijigimg {
+            width: 50px;
+            height: 50px;
+            background-color: #555;
+            border-radius: 50%;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        .rev {
+            width: 100px;
+            height: 20px;
+            background-color: #555;
+            border-radius: 5px;
+            position: absolute;
+        }
+
+        .text {
+            width: 140px;
+            height: 50px;
+            background-color: #555;
+            border-radius: 5px;
+        }
+
+        .rev-info {
+            margin-left: 10px;
+        }
+
+        .section-neww {
+            margin-top: 20px;
+        }
+
+        .section-revieww {
+            margin-left: 20px;
+            margin-top: 20px;
+        }
+        .egnee{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+        }
+        .next .ug:first-child{
+            margin-right: 200px;
+        }
+        .next .ug:last-child{
+            margin-left: 200px;
+        }
+    </style>
+    <section class="movies-content" id="movies">
+        <div class="slidee"></div>
+        <div class="egnee">
+            <section class="section-neww">
+                <div class="new-movies">
+                    <div class="neg"></div>
+                    <div class="neg"></div>
+                    <div class="neg"></div>
+                </div>
+            </section>
+
+            <div class="section-revieww">
+                <div class="review">
+                    <div class="article">
+                        <div class="img"></div>
+                        <div class="rev-info">
+                            <div class="h3"></div>
+                            <div class="jijigimg"></div>
+                            <div class="text"></div>
+                        </div>
+                    </div>
+                    <div class="article">
+                        <div class="img"></div>
+                        <div class="rev-info">
+                            <div class="h3"></div>
+                            <div class="jijigimg"></div>
+                            <div class="text"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        `;
+    
+        this.newMoviesSection.innerHTML = skeletonLoadingHTML;
+    }
+    
+      hideSkeletonLoading() {
+        const skeletonLoading = this.newMoviesSection.querySelector('.skeleton-loading');
+        if (skeletonLoading) {
+            skeletonLoading.remove();
+        }
+    }
+
     displayMoviesNew(container, movies) {
-        this.displayMovies(container, movies, container.classList.contains('new-movies') ? 3 : movies.length);
+        this.displayMovies(container, movies, container.classList.contains('new-movies') ? 4 : movies.length);
     }
 
     displayMoviesTop(container, movies) {
